9e907af773bb4321a40e801de7cf1799905ce9b5
Carlos <carlos-84@live.com>
2022-02-01 22:11:29 -0300

    ------------

---
+++ Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparacaoObjeto.Entidades
+{
+    internal class Funcionario : IComparable
+    {
+        public string nome;
+        public double salario;
+
+        public Funcionario(string csvFuncionario)
+        {
+            string [] vetor = csvFuncionario.Split(',');
+            nome = vetor[0];
+            salario = double.Parse(vetor[1]);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Funcionario))
+            {
+                throw new ArgumentException("Objeto passado não existe!!!");
+            }
+            Funcionario outro = obj as Funcionario;
+            //Comparar nome
+            //return nome.CompareTo(outro.nome);
+            return outro.salario.CompareTo(salario);
+        }
+
+        public override string ToString()
+        {
+            return nome + ", " + salario.ToString("F2");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using ComparacaoObjeto.Entidades;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparacaoObjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\cSharpDelloite\arquivo1.txt";
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionario> list = new List<Funcionario>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Funcionario(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Funcionario s in list)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
